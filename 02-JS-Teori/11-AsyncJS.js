c19220e71ecfc011f1fdefde149a5e642320a288
Hakan Girgin <hakangirgin7@gmail.com>
2024-07-04 12:33:15 +0300

    async yapılar fetch kullanımları ile ilgili çalışmalar

--- 02-JS-Teori/11-AsyncJS.js
+++ 02-JS-Teori/11-AsyncJS.js
@@ -87,27 +87,155 @@
 // xhr.responseType="json";
 // xhr.send();
 
-const url_todo = "https://jsonplaceholder.typicode.com/todos";
-const xhr= new XMLHttpRequest();
-const data ={
-    userId:999,
-    id:999,
-    title:"ANK-16",
-    completed:true
-}
-console.log(data);
+// const url_todo = "https://jsonplaceholder.typicode.com/todos";
+// const xhr= new XMLHttpRequest();
+// const data ={
+//     userId:999,
+//     id:999,
+//     title:"ANK-16",
+//     completed:true
+// }
+// console.log(data);
 
-const newPost= JSON.stringify(data);
-console.log(newPost);
+// const newPost= JSON.stringify(data);
+// console.log(newPost);
 
-xhr.onreadystatechange= ()=>{
-    if (xhr.readyState===4 && xhr.status===201) {
-        console.log("4-DONE");
-        console.log(xhr.response);
-        console.log(xhr.status);
-    }
+// xhr.onreadystatechange= ()=>{
+//     if (xhr.readyState===4 && xhr.status===201) {
+//         console.log("4-DONE");
+//         console.log(xhr.response);
+//         console.log(xhr.status);
+//     }
+// }
+// xhr.open("POST",url_todo);
+// xhr.setRequestHeader("Content-type","application/json;charset=utf-8");
+// xhr.responseType="json";
+// xhr.send(newPost);
+//------------------------------------------------------------------
+//ES-6 Promise ve Fetch
+
+// const promiseObject = new Promise((resolve,reject)=>{
+//     // resolve("Olumlu Sonuç");
+//     reject("Oooops Hata!!!!");
+// })
+// .then(sonuc=>console.log(sonuc))// olumlu sonucu yakalıyoruz
+// .catch(err=>console.log(err));//olumsuz sonucu yakalıyoruz
+
+// console.log(promiseObject);
+//------------------------------------------------------------------
+//Fetch API
+//fetch api bir promise objesi döndürür.
+
+// const url_user = "https://jsonplaceholder.typicode.com/users";
+// const fetchApi = fetch(url_user)//Get istediğini fetch ile tek satırda atmış olduk.
+// .then(response=>response.json())//buradan gelen json objesi ile ilgili işlemleri ikinci then fonksiyonuyla manipule edebiliyoruz. Then fonksiyonundan dönen değer bir Promise objesidir.1. Then metodundan dönen sonuc 2. then fonksiyonunun parametresi olmuş olur.
+// .then(data=>console.log(data))
+// .catch(err=>console.log(err));
+// console.log(fetchApi);
+
+//------------------------------------------------------------------
+
+// const url_post = "https://jsonplaceholder.typicode.com/posts";
+// const data = {
+//     userId:"333",
+//     title:"ANK-16",
+//     body:"Boost Yıldız Yazılımcılar"
+// }
+// const newPost = JSON.stringify(data);
+// const fetchPost = fetch(url_post,{
+//     method:"POST",
+//     body:newPost,
+//     headers:{"Content-type" : "application/json; charset=UTF-8"}
+// })
+// .then(res=>res.json())
+// .then(data=>console.log(data))
+// .catch(err=>console.log(err));
+// console.log(fetchPost);
+
+//------------------------------------------------------------------
+const url_post = "https://jsonplaceholder.typicode.com/posts";
+// const data = {
+//     userId:"333",
+//     title:"ANK-16",
+//     body:"Boost Yıldız Yazılımcılar"
+// }
+// const newPost = JSON.stringify(data);
+// const fetchPost = fetch(url_post,{
+//     method:"POST",
+//     body:newPost,
+//     headers:{"Content-type" : "application/json; charset=UTF-8"}
+// })
+// .then(res=>res.json())
+// .then(data=>{
+//     console.log(data);
+//     return data.userId;
+// })
+// .then(data2=>Number(data2)/111)
+// .then(result=>console.log(result**2))
+// .catch(err=>console.log(err));
+// console.log(fetchPost);
+
+//------------------------------------------------------------------
+
+// const pokeUrl = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1302";
+
+// const getPoke = fetch(pokeUrl)
+// .then(res=>res.json())
+// .then(data=>{
+//     console.log(data.results[1260].name)
+//     console.log(data.results[1260].url)
+//     let url_2= data.results[1260].url
+//     fetch(url_2)
+//     .then(res2=>res2.json())
+//     .then(data2=>{
+//         console.log(data2);
+//         console.log(data2.sprites)
+//         console.log(data2.sprites.other);
+//         console.log(data2.sprites.other["official-artwork"].front_default);
+
+
+//     })
+// })
+//------------------------------------------------------------------
+
+const pokeUrl = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1302";
+
+// const getPoke = fetch(pokeUrl)
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data.results[1260].name)
+//         console.log(data.results[1260].url)
+//         let url_2 = data.results[0].url
+//         return fetch(url_2)
+//     })
+//     .then(res2 => res2.json())
+//     .then(data2 => {
+//         console.log(data2);
+//         console.log(data2.sprites)
+//         console.log(data2.sprites.other);
+//         console.log(data2.sprites.other["official-artwork"].front_default);
+//     })
+
+//------------------------------------------------------------------
+//async - await
+// async bir fonksiyon olduğunu belirtmediğimiz için veri değişkeni onu beklemediği için TypeError: response.json is not a function hatası alırız. response değişkeni çalışmadan veri değişkeninin olduğu yer çalışmaması gerekmektedir. Bu yüzden  fonksiyonun adına async yazıp bekletmek istediğimiz satırlara await yazmamız gerekiyor.
+// const asyncGet = async () => {
+//     const response = await fetch(url_post)
+//     const veri = await response.json();
+//     console.log(veri);
+// }
+// asyncGet();
+
+//------------------------------------------------------------------
+const asyncGetPoke = async () =>{
+    const response = await fetch(pokeUrl)
+    const data = await response.json();
+    console.log(data.results[7].name);
+    const url2= data.results[7].url;
+    const response2= await fetch(url2);
+    const data2 = await response2.json();
+    console.log(data2.sprites.other["official-artwork"].front_default);
 }
-xhr.open("POST",url_todo);
-xhr.setRequestHeader("Content-type","application/json;charset=utf-8");
-xhr.responseType="json";
-xhr.send(newPost);
+asyncGetPoke();
+
+//------------------------------------------------------------------
