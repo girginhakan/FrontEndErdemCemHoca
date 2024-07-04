8d2136c323039474a723860b492a846ad34cd769
Hakan Girgin <hakangirgin7@gmail.com>
2024-07-04 17:03:45 +0300

    high orderArray metodlarla ilgili çalışmalar

--- 02-JS-Uygulama/06-Uygulama0307.js
+++ 02-JS-Uygulama/06-Uygulama0307.js
@@ -10,19 +10,19 @@ let cumleninKelimeSayisi = cumle => cumle.split(' ').length;
 console.log(cumleninKelimeSayisi(cumle));
 // 3- Verilen bir sayının faktöriyelini hesaplayan bir fonksiyon yazın. (Function Declaration Yöntemi ile)
 
-function FaktoriyelHesaplama(sayi3) {
-    let faktoriyel = 1;
-    for (let i = 1; i <= sayi3; i++) {
-        faktoriyel = faktoriyel * i;
-    }
-    return faktoriyel;
+function faktoriyelHesaplama(sayi3) {
+  let faktoriyel = 1;
+  for (let i = 1; i <= sayi3; i++) {
+    faktoriyel = faktoriyel * i;
+  }
+  return faktoriyel;
 }
 
 console.log(FaktoriyelHesaplama(4));
 
 // 4- Verilen bir sayının asal olup olmadığını kontrol eden bir fonksiyon yazın. (Function Expression Yöntemi ile)
 
-// let AsalMi = function (sayi) {
+// const AsalMi = function (sayi) {
 //     if (sayi <= 1) return false;
 //     if (sayi <= 3) return true;
 //     if (sayi % 2 === 0 || sayi % 3 === 0) return false;
@@ -51,24 +51,24 @@ console.log(FaktoriyelHesaplama(4));
 
 // 5- Verilen bir dizi içindeki en büyük sayıyı bulan bir fonksiyon yazın.
 
-let dizi =[1,12,67,29,35];
+let dizi = [1, 12, 67, 29, 35];
 
-// let enBuyukSayi = function(dizi) {
+// const enBuyukSayi = function(dizi) {
 //     return Math.max(...dizi);
 //   };
-  
+
 //   console.log(enBuyukSayi(dizi)); 
-  
+
 
 // 6- Verilen bir dizi içindeki sayıların toplamını hesaplayan bir fonksiyon yazın.
 
-let dizi2 =[1,12,67,29,35];
+let dizi2 = [1, 12, 67, 29, 35];
 
-// let diziToplami = function(dizi) {
+// const diziToplami = function(dizi) {
 //     return dizi.reduce((toplam, sayi) => toplam + sayi);
 //   };
-  
-  
+
+
 //   console.log(diziToplami(dizi2));
 
 // 7-Yukarıdaki son iki fonksiyonu arrow function olarak yeniden yazın.
@@ -82,24 +82,55 @@ console.log(diziToplami(dizi2));
 
 // 8- Verilen bir stringi tersine çeviren bir fonksiyon yazın.
 
-const kelimenintersi= kelime=>kelime.split('').reverse().join('');
+const kelimenintersi = kelime => kelime.split('').reverse().join('');
 
-let kelime="Hakan";
+let kelime = "Hakan";
 console.log(kelimenintersi(kelime));
 
 
 // 9- Verilen bir dizinin elemanlarını rastgele karıştıran bir fonksiyon yazın.
 
 const rastgeleKaristir = dizi => {
-    for (let i = dizi.length - 1; i > 0; i--) {
-      const j = parseInt(Math.random() * (i + 1));
-      [dizi[i],dizi[j]] = [dizi[j], dizi[i]];
-    }
-    return dizi;
-  };
-  
-  const sayilar = [1, 2, 3, 4, 5];
-  console.log(rastgeleKaristir(sayilar));
+  for (let i = dizi.length - 1; i > 0; i--) {
+    const j = parseInt(Math.random() * (i + 1));
+    [dizi[i], dizi[j]] = [dizi[j], dizi[i]];
+  }
+  return dizi;
+};
+const sayilar = [1, 2, 3, 4, 5];
+console.log(rastgeleKaristir(sayilar));
+
 // 10- Verilen bir sayının pozitif bölenlerini bulan bir fonksiyon yazın.(örn. 12'nin pozitif bölenleri 1,2,3,4,6,12)
+let sayi = 30;
+const pozitifBolenler = [];
+const pozitifTamBolenler = sayi => {
+  for (let i = 2; i < sayi; i++) {
+    if (sayi % i === 0) {
+      pozitifBolenler.push(i);
+    }
+  }
+  return pozitifBolenler;
+}
+console.log(pozitifTamBolenler(sayi));
 // 11- Verilen bir stringin içinde belirli bir karakterin kaç kez tekrarlandığını hesaplayan bir fonksiyon yazın.
+
+let kelime1 = "Hakan";
+let harf="a"
+
+const tekrarEdenKarakterSayisi = (kelime,harf) =>{
+  let count =0;
+  for (let i = 0; i <= kelime.length; i++) {
+    if (kelime[i]===harf) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(tekrarEdenKarakterSayisi(kelime1,harf));
+
 // 12 - içerisine kaç adet sayı yazılırsa yazılsın, sayıların toplamını döndüren bir fonksiyon yazınız.
+
+const sayilarinToplami = (...sayilar)=>{
+  return sayilar.reduce((toplam, sayi) => toplam + sayi);
+}
+console.log(sayilarinToplami(1,2,3,4,5));
---
+++ 02-JS-Uygulama/07-Uygulama0407.js
@@ -0,0 +1,139 @@
+// Kullanılacak diziler
+const dizi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const dizi1 = ["elma", "armut", "kiraz", "üzüm"];
+const dizi2 = [1, 2, -3, 4, -5, 6, 7, 8, 9, 10];
+const dizi3 = [2, 5, 3, 4, 7, 10, 1, 9, 8, 6];
+
+// ---------------forEach-------------------
+// 1- Verilen bir dizinin tüm elemanlarını ekrana yazdıran bir forEach döngüsü oluşturun.
+
+dizi.forEach(eleman =>console.log("Dizi : " + eleman));
+// dizi1.forEach(eleman =>console.log("Dizi1 : " + eleman));
+// dizi2.forEach(eleman =>console.log("Dizi2 : " + eleman));
+// dizi3.forEach(eleman =>console.log("Dizi3 : " + eleman));
+
+// 2- Verilen bir dizinin sadece çift sayılarını ekrana yazdıran bir forEach döngüsü oluşturun.
+
+dizi.forEach(ciftSayilar=>{
+    if (ciftSayilar%2===0) {
+        console.log(ciftSayilar);
+    }
+});
+
+// 3- Verilen bir dizinin elemanlarını 2 ile çarpan ve sonucu yeni bir diziye ekleyen bir forEach döngüsü oluşturun.
+let elemaninIkiKati = [];
+dizi.forEach(eleman => {
+    elemaninIkiKati.push(eleman * 2);
+});
+console.log(`Dizinin elemanlarının iki katı : ${elemaninIkiKati}`);
+
+// 4- Verilen bir dizinin elemanlarının toplamını hesaplayan bir forEach döngüsü oluşturun.(reduce kullanmadan)
+let toplam =0;
+dizi.forEach(eleman=>{
+    toplam += eleman;
+})
+console.log("Dizinin elemanları toplamı : " + toplam);
+
+// 5- Verilen bir dizinin elemanlarını ekrana yazdırırken, dizinin son elemanını özel bir mesajla birlikte yazdıran bir forEach döngüsü oluşturun.
+
+dizi1.forEach((eleman,index)=>{
+    
+    if (index===dizi1.length-1) {
+        console.log("En sevdiğim meyve : " + eleman);
+    }else{
+        console.log(eleman);
+    }
+})
+
+
+//Javascriptte Foreach ile map arasındaki tek fark foreach array dönmez ama map array döner.
+// ---------------map-------------------
+// 6- Verilen bir dizideki elemanları büyük harf yaparak yeni bir dizi oluşturan bir map döngüsü oluşturun.
+
+const buyukHarfliDizi = dizi1.map(eleman=>eleman.toUpperCase());
+console.log(buyukHarfliDizi);
+
+// 7- Verilen bir dizinin elemanlarını 1 ile 10 arasında rastgele bir sayıyla toplayıp yeni bir dizi oluşturan bir map döngüsü oluşturun.
+const karisikDizi=[];
+let rastgeleSayi=parseInt(Math.random()*10+1);
+console.log(rastgeleSayi);
+karisikDizi = dizi.map(eleman=>eleman+rastgeleSayi);
+console.log(karisikDizi);
+
+
+// 8- Verilen bir stringin her bir kelimesini ters çevirerek yeni bir dizi oluşturan bir map döngüsü oluşturun.
+
+const tersKelimeliDizi = dizi1.map(kelime=>kelime.split("").reverse().join(""));
+console.log(tersKelimeliDizi);
+
+// 9- Verilen bir dizinin elemanlarını(elemanlar sayı olmalı) stringe çevirerek yeni bir dizi oluşturan bir map döngüsü oluşturun.
+
+const stringeDonmusDizi = dizi.map(eleman=>eleman.toString());
+console.log(stringeDonmusDizi);
+
+// 10- Verilen bir dizi içindeki stringleri ters çevirerek yeni bir dizi oluşturan bir map döngüsü oluşturun. Ancak, bu sefer reverse() yerine kendi ters çevirme algoritmanızı oluşturun.
+
+const tersCevirme = kelime =>{
+    let sondakiHarf="";
+    for (let i = kelime.length-1; i >= 0; i--) {
+        sondakiHarf+= kelime[i];
+    }
+    return sondakiHarf;
+};
+
+const tersCevrilmisDizi = dizi1.map(eleman=>tersCevirme(eleman));
+console.log(tersCevrilmisDizi);
+// ---------------filter-------------------
+// 11- Verilen bir dizi içerisindeki çift sayıları filtreleyen bir örnek yazın.
+
+const ciftSayiliDizi = dizi.filter(cift=>cift%2===0);
+console.log(ciftSayiliDizi);
+
+// 12- Verilen bir dizi içerisindeki stringlerden belirli bir uzunluğa sahip olanları filtreleyen bir örnek yazın.
+
+const filtreliDizi = dizi1.filter(karakter=>karakter.length>4)
+console.log(filtreliDizi);
+
+// ---------------reduce-------------------
+// 13- Verilen bir dizi içerisindeki sayıların toplamını, her bir sayıya 2 ekleyerek hesaplayan bir örnek yazın.
+
+
+const atrmisDizi = dizi.reduce((toplam, sayi) => toplam + (sayi + 2),0)
+
+console.log(atrmisDizi);
+
+
+// ---------------find-------------------
+// 14- Verilen bir dizi içerisindeki ilk çift sayıyı döndüren bir örnek
+
+console.log(dizi.find(sayi=>sayi%2===0));
+
+
+// 15- Verilen bir dizi içerisindeki son çift sayıyı döndüren bir örnek
+
+function sonCiftSayi(dizi) {
+    let ciftSayilar = dizi.filter(num => num % 2 === 0);
+    return ciftSayilar.pop();
+}
+
+let sonCift = sonCiftSayi(dizi);
+console.log("Son çift sayı:", sonCift);
+
+
+
+
+// ---------------some-------------------
+// 16- Verilen bir dizi içerisinde en az bir negatif sayı olup olmadığını kontrol eden bir örnek
+
+console.log(dizi2.some(eleman=>eleman<0));
+
+
+// ---------------every-------------------
+// 17- Verilen bir dizi içerisindeki tüm sayıların pozitif olduğunu kontrol eden bir örnek
+
+console.log(dizi.every(eleman=>eleman<0));
+
+// ---------------sort-------------------
+// 18- Verilen bir dizi içerisindeki sayıları sıralayan bir örnek
+
+console.log(dizi2.sort((a, b) => a - b));
