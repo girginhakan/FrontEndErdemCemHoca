b56b8a3832751bbf3bd89b72609950a216bc53df
Hakan Girgin <hakangirgin7@gmail.com>
2024-07-17 12:51:09 +0300

    blog-project mini proje yapıldı. react-router ile ilgili çalışmalar yapıldı

---
+++ 03-REACT.JS-Teori/blog-project/src/components/Anasayfa.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Anasayfa = () => {
+    let routerPic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ9u8cqRpzhIGF-tXlPw0Lvwj4mzm9cXXidw&s";
+  return (
+    <div className='main'>
+        <h3>Ank-16 React Router</h3>
+        <img src={routerPic} alt="" className='router' />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatum iure dolores vel repudiandae, quibusdam dicta impedit consequuntur veniam eum! Quidem, non dicta. Quis, ea nihil debitis sint veniam assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus iste recusandae corporis modi aperiam dignissimos illo officia quas sint assumenda. Necessitatibus, a. Voluptas error incidunt pariatur. Quos culpa et vel? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus nihil velit eum distinctio eveniet unde praesentium? Quae nulla reprehenderit dolorem magni consectetur earum, deleniti quidem, doloribus quibusdam ipsum nisi itaque!</p>
+    </div>
+  )
+}
+
+export default Anasayfa
---
+++ 03-REACT.JS-Teori/blog-project/src/components/Iletisim.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Iletisim = () => {
+  return (
+    <div className='contact'>
+        <h3>Bize Ulaşın</h3>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2485.539489917619!2d-2.5799951!3d51.466611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48718e134435bc83%3A0xe5190fa424c97223!2sSuper-Tonic%20Barbers%20Bristol!5e0!3m2!1str!2str!4v1721199105223!5m2!1str!2str" width="600" height="450"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  )
+}
+
+export default Iletisim
