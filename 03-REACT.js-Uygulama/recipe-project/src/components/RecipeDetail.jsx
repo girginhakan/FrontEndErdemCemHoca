528256432ede03e9dc86e828b20088a72f3f1f03
Hakan Girgin <hakangirgin7@gmail.com>
2024-07-18 09:13:19 +0300

    recipe-project detay sayfası ile ilgili çalışmalar yapıldı

--- 03-REACT.js-Uygulama/recipe-project/src/components/RecipeDetail.jsx
+++ 03-REACT.js-Uygulama/recipe-project/src/components/RecipeDetail.jsx
@@ -2,17 +2,15 @@ import React, { useContext } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import DataContext from '../context/DataContext';
 
-const RecipeDetail = ({recipe}) => {
-  // const { setTitle,
-  //   setDescription,
-  //   setImage}=useContext(DataContext);
-  // const params =useParams();
-  // const parametre =params.recipesId;
+const RecipeDetail = () => {
+  const {recipes}=useContext(DataContext);
+  const params =useParams();
+  const parametre =params.recipeId;
   return (
     <div className='detail'>
-        <img src={recipe.image} alt="" />
-        <h3>{recipe.title}</h3>
-        <p>{recipe.description}</p>
+        <img src={recipes[parametre-1].image} alt="" />
+        <h3>{recipes[parametre-1].title}</h3>
+        <p>{recipes[parametre-1].description}</p>
         <Link to="/recipe-app/recipelist">Return All Recipes</Link>
     </div>
   )
